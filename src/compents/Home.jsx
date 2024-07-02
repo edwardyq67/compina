f603be927cec66e22f1cc3f57fd201685db12377
edwardyq67 <edwardyq67@gmail.com>
2024-07-02 01:12:06 -0500

    actualizacion9

--- src/compents/Home.jsx
+++ src/compents/Home.jsx
@@ -53,13 +53,13 @@ function Home() {
           disableOnInteraction: false,
         }}
         modules={[Autoplay, EffectFade, Navigation, Pagination]}
-        className="h-[60vh] mySwiper"
+        className="h-[80vh] mySwiper"
 
       >
         <SwiperSlide className='relative'>
           <img className='w-full h-full object-cover' src={portadaImg1} />
 
-          <div className="absolute  rounded-md  inset-y-32  right-0 sm:right-2 lg:right-10">
+          <div className="absolute  rounded-md  bottom-2  right-0 sm:right-2 lg:right-10">
             <div className="p-2 sm:p-4 bg-mysin-400 max-w-[600px] rounded-md">
               <p className='text-4xl md:text-5xl lg:text-7xl text-white border-8 rounded border-white p-5 lg:p-10 font-semibold'>Commercial, logistical and marketing partner</p>
             </div>
@@ -68,7 +68,7 @@ function Home() {
         </SwiperSlide>
         <SwiperSlide>
           <img className='w-full h-full object-cover' src={portadaImg2} />
-          <div className="absolute rounded-md bottom-2 md:inset-y-32 right-0 sm:right-2 lg:right-10 px-2">
+          <div className="absolute rounded-md bottom-2  right-0 sm:right-2 lg:right-10 px-2">
               <div className="p-1 sm:p-4 bg-mysin-400 max-w-[600px]  rounded-md">
                 <p className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white border-4 md:border-6 lg:border-8 rounded border-white p-1  md:p-5 sm:p-3 lg:p-10 font-semibold'>
                   Communicating Sustainable Marketing and Experiences
@@ -78,7 +78,7 @@ function Home() {
         </SwiperSlide>
         <SwiperSlide>
           <img className='w-full h-full object-cover' src={portadaImg3} />
-          <div className="absolute rounded-md bottom-2 md:inset-y-32 right-0 sm:right-2 lg:right-10 px-2">
+          <div className="absolute rounded-md bottom-2 lg:top-8  right-0 sm:right-2 lg:right-10 px-2">
               <div className="p-1 sm:p-4 bg-mysin-400 max-w-[600px]  rounded-md">
                 <p className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white border-4 md:border-6 lg:border-8 rounded border-white p-1  md:p-5 sm:p-3 lg:p-10 font-semibold'>
                   Sustainable Management, Environmental Preservation, Native Culture and Tourism
@@ -99,7 +99,7 @@ function Home() {
         </SwiperSlide>
 
       </Swiper>
-      <section className="container mx-auto pt-10 lg:pt-20 px-2 divide-y-2 mb-10">
+      <section className="container mx-auto pt-10 px-2 divide-y-2 mb-10">
         <div className="grid grid-cols-4 gap-6 ">
           <div className="col-span-4 mx-auto"><h2 className='text-4xl md:text-5xl text-mysin-500 font-semibold'>our services</h2> </div>
           <p className='max-w-[900px] mx-auto col-span-4 text-center text-lg md:text-xl text-mysin-400 font-light'>Our services are strategically coordinated and tailored to meet your sustainability goals, with a primary focus on fostering corporate growth and enhancing brand recognition for our clients.</p>
